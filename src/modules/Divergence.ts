0a69bdc97b74a4c4bc2d7e4969f233a8791c1933
koudaiishigame <kira5052@gmail.com>
2022-03-06 18:28:37 +0900

    rewrite Divergence

--- src/modules/Divergence.ts
+++ src/modules/Divergence.ts
@@ -2,9 +2,17 @@ import face_vert from "./glsl/sim/face.vert?raw";
 import divergence_frag from "./glsl/sim/divergence.frag?raw";
 
 import ShaderPass from "./ShaderPass";
+import { SimProps } from "../types/Sim";
+
+interface Props extends SimProps {
+  boundarySpace: THREE.Vector2;
+  dst: THREE.WebGLRenderTarget;
+  src: THREE.WebGLRenderTarget;
+  dt: number;
+}
 
 export default class Divergence extends ShaderPass {
-  constructor(simProps) {
+  constructor(simProps: Props) {
     super({
       material: {
         vertexShader: face_vert,
@@ -30,8 +38,10 @@ export default class Divergence extends ShaderPass {
     this.init();
   }
 
-  update({ vel }) {
-    this.uniforms.velocity.value = vel.texture;
+  updateDivergence({ vel }: { vel: THREE.WebGLRenderTarget }) {
+    if (this.uniforms) {
+      this.uniforms.velocity!.value = vel.texture;
+    }
     super.update();
   }
 }
